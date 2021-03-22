1c65456226dedd26a082ff3f38720ce48b6bf24f
codecowboydotio <scott@vankalken.org>
2021-03-22 11:07:41 +1100

    updated vars file

---
+++ as3_bigip_bigiq/vars.tf
@@ -0,0 +1,31 @@
+variable "bigip_address" {
+  default = "x.x.x.x"
+}
+variable "bigip_username" {
+  default = "admin"
+}
+variable "bigip_password" {
+  default = "xxx"
+}
+variable "bigip_port" {
+  default = "8443"
+}
+variable "bigip_private_address" {
+  default = "x.x.x.x"
+}
+variable "consul_private_address" {
+  default = "x.x.x.x"
+}
+###
+variable "bigiq_address" {
+  default = "x.x.x.x"
+}
+variable "bigiq_username" {
+  default = "admin"
+}
+variable "bigiq_password" {
+  default = "xxxx"
+}
+variable "bigiq_port" {
+  default = "443"
+}
