2042d0710f1aac8c0d9b9ad718e4772b6f12cec7
codecowboydotio <scott@vankalken.org>
2021-09-02 08:19:30 +1000

    updated frontend single site volterra manifest

--- volterra/single-site/vars.tf
+++ volterra/single-site/vars.tf
@@ -1,18 +1,18 @@
 variable "ns" { default = "s-vankalken" }
 variable "domain" { default = "sa.f5demos.com" }
 variable "servicename" { default = "svk-swapi-api1" }
-variable "vehicles_servicename" { default = "svk-swapi-vehicles1" }
-variable "starships_servicename" { default = "svk-swapi-starships1" }
-variable "people_servicename" { default = "svk-swapi-people1" }
-variable "planets_servicename" { default = "svk-swapi-planets1" }
-variable "frontend_servicename" { default = "svk-swapi-frontend1" }
+variable "vehicles_servicename" { default = "svk-swapi-vehicles2" }
+variable "starships_servicename" { default = "svk-swapi-starships2" }
+variable "people_servicename" { default = "svk-swapi-people2" }
+variable "planets_servicename" { default = "svk-swapi-planets2" }
+variable "frontend_servicename" { default = "svk-swapi-frontend2" }
 
-variable "manifest_app_name" { default = "svk-swapi-api1" }
-variable "frontend_manifest_app_name" { default = "svk-swapi-frontend1" }
-variable "vehicles_manifest_app_name" { default = "svk-swapi-vehicles1" }
-variable "starships_manifest_app_name" { default = "svk-swapi-starships1" }
-variable "people_manifest_app_name" { default = "svk-swapi-people1" }
-variable "planets_manifest_app_name" { default = "svk-swapi-planets1" }
+variable "manifest_app_name" { default = "svk-swapi-api2" }
+variable "frontend_manifest_app_name" { default = "svk-swapi-frontend2" }
+variable "vehicles_manifest_app_name" { default = "svk-swapi-vehicles2" }
+variable "starships_manifest_app_name" { default = "svk-swapi-starships2" }
+variable "people_manifest_app_name" { default = "svk-swapi-people2" }
+variable "planets_manifest_app_name" { default = "svk-swapi-planets2" }
 
 variable "site_selector" { default = [ "ves.io/siteName in (ves-io-wes-sea, ves-io-ny8-nyc)" ] }
 
