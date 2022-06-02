729999a5892b255610c1c810ce65b994499d61c2
codecowboydotio <scott@vankalken.org>
2022-06-03 08:06:07 +1000

    updated webhook

--- webhook/lambda_code/webhook.py
+++ webhook/lambda_code/webhook.py
@@ -1,7 +1,21 @@
-import boto3
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
-    result = "Hello World"
+    raw_event = event['body']
+    logger.info(event['body'])
+    json_event = json.loads(raw_event)
+    logger.info(type(json_event))
+    logger.info('Category: %s', json_event['category'])
+    logger.info('Cloud Provider: %s', json_event['cloud_provider'])
+    logger.info('Account Name: %s', json_event['account_name'])
+    logger.info('Alert ID: %s', json_event['state']['alert_id'])
+    logger.info('Severity: %s', json_event['state']['severity'])
     return {
-        'statusCode' : 200,
-        'body': result
+        'statusCode' : '200',
+        #'body': json.dumps(event)
+        'body': json.dumps(json_event)
     }
