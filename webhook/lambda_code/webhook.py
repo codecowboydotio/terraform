fe16ea3480b3f0b1317797f47bd4251ab698e67a
codecowboydotio <scott@vankalken.org>
2022-06-03 12:24:19 +1000

    webhook updates

--- webhook/lambda_code/webhook.py
+++ webhook/lambda_code/webhook.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import logging
 
 logger = logging.getLogger()
@@ -14,6 +15,26 @@ def lambda_handler(event, context):
     logger.info('Account Name: %s', json_event['account_name'])
     logger.info('Alert ID: %s', json_event['state']['alert_id'])
     logger.info('Severity: %s', json_event['state']['severity'])
+    # curl -X POST https://webhook-worker.scottvankalken.workers.dev/ -H "Content-Type: application/json" -d '{"foo":"bar"}'
+    ####
+      
+    # defining the api-endpoint 
+    API_ENDPOINT = "https://webhook-worker.scottvankalken.workers.dev/"
+      
+    # your API key here
+    API_KEY = "XXXXXXXXXXXXXXXXX"
+      
+    headers = {"content-type":"application/json"}
+    data = {"Category":"paste"}
+      
+    # sending post request and saving response as response object
+    r = requests.post(url = API_ENDPOINT, data = json.dumps(data), headers = headers)
+      
+    # extracting response text 
+    api_response = r.text
+    logger.info('Response from external API: %s', api_response)
+    ####
+
     return {
         'statusCode' : '200',
         #'body': json.dumps(event)
