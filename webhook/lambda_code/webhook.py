c7b6f4cbdc3d7d2c826a9b29d7c4f577c1887bac
codecowboydotio <scott@vankalken.org>
2022-06-06 18:05:12 +1000

    windows testing

--- webhook/lambda_code/webhook.py
+++ webhook/lambda_code/webhook.py
@@ -15,25 +15,29 @@ def lambda_handler(event, context):
     logger.info('Account Name: %s', json_event['account_name'])
     logger.info('Alert ID: %s', json_event['state']['alert_id'])
     logger.info('Severity: %s', json_event['state']['severity'])
-    # curl -X POST https://webhook-worker.scottvankalken.workers.dev/ -H "Content-Type: application/json" -d '{"foo":"bar"}'
-    ####
       
+    # Let's do a POST to an external API endpoint with data
     # defining the api-endpoint 
     API_ENDPOINT = "https://webhook-worker.scottvankalken.workers.dev/"
       
     # your API key here
     API_KEY = "XXXXXXXXXXXXXXXXX"
       
+    # define headers and data portions
     headers = {"content-type":"application/json"}
-    data = {"Category":"paste"}
+    data = {'Category':json_event['category'],
+            'Cloud Provider': json_event['cloud_provider'],
+            'Account Name':json_event['account_name'],
+            'Alert ID':json_event['state']['alert_id'],
+            'Severity':json_event['state']['severity']}
       
     # sending post request and saving response as response object
+    # inside lambda we need to encode the data object as json before sending
     r = requests.post(url = API_ENDPOINT, data = json.dumps(data), headers = headers)
       
     # extracting response text 
     api_response = r.text
     logger.info('Response from external API: %s', api_response)
-    ####
 
     return {
         'statusCode' : '200',
---
+++ windows/aws.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+resource "aws_vpc" "vpc-a" {
+  cidr_block       = var.vpc-a_cidr_block
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    for k, v in merge({
+      app_type = "production"
+      Name = "${var.name-prefix}-${var.project}-vpc-a"
+    },
+    var.default_ec2_tags): k => v
+  }
+}
+
+resource "aws_subnet" "vpc-a_subnet_1" {
+  vpc_id = aws_vpc.vpc-a.id
+  cidr_block = var.vpc-a_subnet_1
+  availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    for k, v in merge({
+      app_type = "production"
+      Name = "${var.name-prefix}-${var.project}-vpc-a-subnet_1-tf"
+    },
+    var.default_ec2_tags): k => v
+  }
+}
+
+resource "aws_internet_gateway" "vpc-a_igw" {
+  vpc_id = aws_vpc.vpc-a.id
+
+  tags = {
+    for k, v in merge({
+      app_type = "production"
+      Name = "${var.name-prefix}-${var.project}-vpc-a-igw-tf"
+    },
+    var.default_ec2_tags): k => v
+  }
+}
+
+resource "aws_route_table" "vpc-a-route-table-tf" {
+  vpc_id = aws_vpc.vpc-a.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc-a_igw.id
+  }
+  tags = {
+    for k, v in merge({
+      app_type = "production"
+      Name = "${var.name-prefix}-${var.project}-vpc-a-rt-tf"
+    },
+    var.default_ec2_tags): k => v
+  }
+}
+
+resource "aws_route_table_association" "vpc-a_subnet_1" {
+  subnet_id = aws_subnet.vpc-a_subnet_1.id
+  route_table_id = aws_route_table.vpc-a-route-table-tf.id
+}
+
+resource "aws_security_group" "vpc-a_allow_all" {
+    vpc_id = aws_vpc.vpc-a.id
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }    
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"        
+        cidr_blocks = ["0.0.0.0/0"]
+    }    
+    tags = {
+      for k, v in merge({
+        app_type = "production"
+        Name = "${var.name-prefix}-${var.project}-vpc-a-sec-tf"
+      },
+      var.default_ec2_tags): k => v
+    }
+}
