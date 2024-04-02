9b4bc4260c6063ee6ebef7e3da5254e57c9be079
codecowboydotio <scottvankalken@gmail.com>
2024-04-02 14:48:11 +1100

    random bucket name

--- webhook/bucket.tf
+++ webhook/bucket.tf
@@ -1,11 +1,17 @@
+resource "random_string" "bucket_suffix" {
+  special = false
+  upper   = false
+  length  = 5
+}
+
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "${var.name-prefix}-${var.project}-a"
+  bucket = "${var.name-prefix}-${var.project}-${random_string.bucket_suffix.id}"
   force_destroy = true
 
   tags = {
     for k, v in merge({
       app_type = "production"
-      Name = "${var.name-prefix}-${var.project}-bucket-a"
+      Name = "${var.name-prefix}-${var.project}-bucket-${random_string.bucket_suffix.id}"
     },
     var.default_ec2_tags): k => v
   }
